de562fa29a9de00b658dfeb9b47021e55b52e43e
Monreal <moncml@outlook.com>
2024-01-25 17:45:20 -0300

    function displayMovements learning: forEach, innerHTML && insertAdjacentHTML

--- script.js
+++ script.js
@@ -61,6 +61,34 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+//Add movements 
+const displayMovements = function(movements){
+
+ //Cleaning the conteiner
+
+//The Element property innerHTML gets or sets the HTML 
+//or XML markup contained within the element.
+//To insert the HTML into the document rather than replace 
+// the contents of an element, use the method insertAdjacentHTML(). -- MDN
+  containerMovements.innerHTML = '';
+
+//Add movements 
+    movements.forEach(function(mov, i){
+      const type = mov > 0 ? 'deposit' : 'withdrawal'
+      const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+      `;
+      //The insertAdjacentHTML() method of the Element interface parses 
+      //the specified text as HTML or XML and inserts the resulting nodes 
+      //into the DOM tree at a specified position. --MDN
+      containerMovements.insertAdjacentHTML('afterbegin', html);
+    });
+};
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
